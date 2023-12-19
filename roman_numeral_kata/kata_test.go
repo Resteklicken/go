0bdf9c89697737033ec1c84e5d9691f1f2ae7762
René Gentzen <rene.gentzen@mittelstand.ai>
2023-12-19 14:33:52 +0000

    Refactored tests
    Made cases slice reusable across en/decoding tests

--- roman_numeral_kata/kata_test.go
+++ roman_numeral_kata/kata_test.go
@@ -5,92 +5,60 @@ import (
 	"testing"
 )
 
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{9, "IX"},
+	{10, "X"},
+	{11, "XI"},
+	{14, "XIV"},
+	{15, "XV"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{41, "XLI"},
+	{44, "XLIV"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{90, "XC"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+}
+
 func TestConversionToRoman(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Want   string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{9, "IX"},
-		{10, "X"},
-		{11, "XI"},
-		{14, "XIV"},
-		{15, "XV"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{41, "XLI"},
-		{44, "XLIV"},
-		{49, "XLIX"},
-		{50, "L"},
-		{100, "C"},
-		{90, "XC"},
-		{400, "CD"},
-		{500, "D"},
-		{900, "CM"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-		{3999, "MMMCMXCIX"},
-		{2014, "MMXIV"},
-		{1006, "MVI"},
-		{798, "DCCXCVIII"},
-	}
 
 	for _, test := range cases {
-		t.Run(fmt.Sprintf("%d -> %q", test.Arabic, test.Want), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d -> %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 
-			if got != test.Want {
-				t.Errorf("got %q want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q want %q", got, test.Roman)
 			}
 		})
 	}
 }
 
 func TestConversionToArabic(t *testing.T) {
-	cases := []struct {
-		Want  int
-		Roman string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{9, "IX"},
-		{10, "X"},
-		{11, "XI"},
-		{14, "XIV"},
-		{15, "XV"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{41, "XLI"},
-		{44, "XLIV"},
-		{49, "XLIX"},
-		{50, "L"},
-		{100, "C"},
-		{90, "XC"},
-		{400, "CD"},
-		{500, "D"},
-		{900, "CM"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-		{3999, "MMMCMXCIX"},
-		{2014, "MMXIV"},
-		{1006, "MVI"},
-		{798, "DCCXCVIII"},
-	}
 	for _, test := range cases {
-		t.Run(fmt.Sprintf("%q -> %d", test.Roman, test.Want), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%q -> %d", test.Roman, test.Arabic), func(t *testing.T) {
 			got := ConvertToArabic(test.Roman)
 
-			if got != test.Want {
-				t.Errorf("got %d want %d", got, test.Want)
+			if got != test.Arabic {
+				t.Errorf("got %d want %d", got, test.Arabic)
 			}
 		})
 	}
